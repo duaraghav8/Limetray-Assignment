dc33765f507159c46460c0f60dd974a6aa707dd8
duaraghav8@gmail <duaraghav8@gmail.com>
2016-02-14 21:04:37 +0530

    Access Control allow Origin Error Fix

--- limetray/app/routes/routes.js
+++ limetray/app/routes/routes.js
@@ -1,6 +1,28 @@
 var controllers = require ('../controllers/controllers');
 
 module.exports = function (app, passport) {
+	//uncomment the chunk below if your browser console throws "Access-Control-Allow-Origin" Error
+
+	/*
+		app.use(function (req, res, next) {
+    		// Website you wish to allow to connect
+    		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + process.env.PORT.toString ());
+
+    		// Request methods you wish to allow
+    		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    		// Request headers you wish to allow
+    		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    		// Set to true if you need the website to include cookies in the requests sent
+    		// to the API (e.g. in case you use sessions)
+    		res.setHeader('Access-Control-Allow-Credentials', true);
+
+    		// Pass to next layer of middleware
+    		next();
+		});
+	*/
+
 	app
 		.get ('/', controllers.loginGet)
 		.get ('/login', controllers.loginGet)
