73438a16b65939c52ebc52985320ca649213147d
Raghav Dua <duaraghav8@gmail.com>
2016-02-14 21:21:07 +0530

    Update routes.js

--- limetray/app/routes/routes.js
+++ limetray/app/routes/routes.js
@@ -6,7 +6,7 @@ module.exports = function (app, passport) {
 	/*
 		app.use(function (req, res, next) {
     		// Website you wish to allow to connect
-    		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + process.env.PORT.toString ());
+    		res.setHeader('Access-Control-Allow-Origin', '*');
 
     		// Request methods you wish to allow
     		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
